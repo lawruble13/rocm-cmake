f95712b37dd3b9535c91c70313816860f097194b
Liam Wrubleski <Liam.Wrubleski@amd.com>
2021-07-29 16:43:17 -0600

    test

--- share/rocm/cmake/ROCMCreatePackage.cmake
+++ share/rocm/cmake/ROCMCreatePackage.cmake
@@ -59,7 +59,6 @@ macro(rocm_create_package)
         set(CPACK_SET_DESTDIR
             ON
             CACHE BOOL "Boolean toggle to make CPack use DESTDIR mechanism when packaging")
-        message(FATAL_ERROR "${CPACK_SET_DESTDIR}")
         if("${CPACK_SET_DESTDIR}")
             set(CPACK_PACKAGING_INSTALL_PREFIX "")
         endif()
