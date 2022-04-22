f7d28e0999e96c20367545d67f3fdce6f3c136d9
Liam Wrubleski <Liam.Wrubleski@amd.com>
2022-04-22 10:13:18 -0600

    Update `original_comments` formatting
    CMake supports multiline strings, and this improves readability and should make the linter less angry.

--- share/rocm/cmake/ROCMHeaderWrapper.cmake
+++ share/rocm/cmake/ROCMHeaderWrapper.cmake
@@ -61,8 +61,14 @@ function(rocm_wrap_header_file)
             get_filename_component(ORIGINAL_FILE_NAME "${ORIGINAL_FILE}" NAME)
             if(INCLUDE_FILE_NAME STREQUAL ORIGINAL_FILE_NAME)
                 file(READ ${ORIGINAL_FILE} file_contents)
-                set(original_contents "#if 0 // Area to dump file contents\n\n/* The following is a copy of the original file for the benefit of build systems which grep for values
-* in this file rather than preprocess it. This is just for backward compatibility */\n\n${file_contents}\n#endif")
+                set(original_contents
+"#if 0 // Area to dump file contents
+
+/* The following is a copy of the original file for the benefit of build systems which grep for values
+ * in this file rather than preprocess it. This is just for backward compatibility */
+
+${file_contents}
+#endif")
             endif()
         endforeach()
         set(GUARD_LIST ${PARSE_GUARDS})
