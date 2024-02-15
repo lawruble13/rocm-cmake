a36e4a89312aa058617aab0cef09f6e11e08c94e
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-02-15 16:47:16 -0700

    Add deprecation message

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -7,6 +7,15 @@ get_filename_component(_new_rocmcmakebuildtools_path "${_new_rocmcmakebuildtools
 # two directories up is sufficient for windows search, but linux search requires the share directory
 get_filename_component(_new_rocmcmakebuildtools_path_linux "${_new_rocmcmakebuildtools_path}" DIRECTORY)
 
+# Emit deprecation message
+set(_rocm_deprecation_message "The 'ROCM' CMake package is deprecated. Please use 'ROCmCMakeBuildTools' instead.")
+if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.17.0")
+    message(DEPRECATION "${_rocm_deprecation_message}")
+else()
+    message(AUTHOR_WARNING "${_rocm_deprecation_message}")
+endif()
+unset(_rocm_deprecation_message)
+
 include(CMakeFindDependencyMacro)
 
 find_dependency(
